37d8d1030231b17365e8b3c05a6c1ec9ba6b79a2
chichAIbelu <etesfaye@hotmail.com>
2023-12-30 21:58:45 +0300

    chore: fetch categories list

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,7 +14,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   bool isFetching = false;
   int hasMore = 0;
   int cursor = 0;
@@ -22,8 +21,10 @@ class _MyHomePageState extends State<MyHomePage> {
   int take = 100;
   bool topLevelOnly = true;
   int totalItems = 0;
-  Input$CollectionSortParameter sort = Input$CollectionSortParameter(createdAt: Enum$SortOrder.ASC);
-  Input$CollectionFilterParameter filter = Input$CollectionFilterParameter(); //parentId: Input$IDOperators (eq:"1")
+  Input$CollectionSortParameter sort =
+      Input$CollectionSortParameter(createdAt: Enum$SortOrder.ASC);
+  Input$CollectionFilterParameter filter =
+      Input$CollectionFilterParameter(); //parentId: Input$IDOperators (eq:"1")
   Enum$LogicalOperator filterOperator = Enum$LogicalOperator.OR;
   late Input$CollectionListOptions input;
   late Variables$Query$GetCollections variables;
@@ -32,14 +33,21 @@ class _MyHomePageState extends State<MyHomePage> {
   late QueryResult<Query$GetCollections> previousResult;
   late QueryResult<Query$GetCollections> allCategories;
 
-  Future<QueryResult<Query$GetCollections>> _getAllCategories(BuildContext context) async {
+  Future<QueryResult<Query$GetCollections>> _getAllCategories(
+      BuildContext context) async {
     isFetching = true;
     cursor = 0;
     take = 100;
     skip = cursor * take;
-    input = Input$CollectionListOptions(skip: skip, take: take, sort: sort, filter: filter, topLevelOnly: topLevelOnly); //, filterOperator: filterOperator
+    input = Input$CollectionListOptions(
+        skip: skip,
+        take: take,
+        sort: sort,
+        filter: filter,
+        topLevelOnly: topLevelOnly); //, filterOperator: filterOperator
     variables = Variables$Query$GetCollections(options: input);
-    options = Options$Query$GetCollections( variables: variables); //, fetchPolicy: FetchPolicy.networkOnly
+    options = Options$Query$GetCollections(
+        variables: variables); //, fetchPolicy: FetchPolicy.networkOnly
     allCategories = await getCollections(input, null, null);
     totalItems = allCategories.parsedData!.collections.totalItems;
     hasMore = totalItems - allCategories.parsedData!.collections.items.length;
@@ -88,8 +96,26 @@ class _MyHomePageState extends State<MyHomePage> {
                   // var collections = snapshot.data?.parsedData?.collections.items;
                   var categories =
                       snapshot.data?.parsedData?.collections.items ?? [];
-                  print('[Category] ${categories.length}');
-                  return const SizedBox();
+                  print('[Category length] ${categories.length}');
+                  
+                  return SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: List.generate(
+                        categories.length,
+                        (index) => Text(
+                          categories[index].name,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 0.5,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                
                 }),
           ],
         ),
